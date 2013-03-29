bc0210a1d8c70e0cd052337bc27328cef7f18570
sanjaya <pandeysanjaya4@gmail.com>
2013-03-29 13:35:30 +0545

    midified all content and landing page

---
+++ nepal khabar/js/relatedvideos.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function switchquestions(id)
+{ 
+
+var idarray= new Array("idone","idtwo", "idthree"); 
+for(i=0; i<thearray.length; i++){ 
+      if(thearray[i] == id){ 
+           document.getElementById(thearray[i]).style.visibility="visible";
+      }    
+      
+      if(thearray[i]!=id){
+                 document.getElementById(thearray[i]).style.visibility="hidden";
+           
+      }
+           
+     
+   } 
+}
+
