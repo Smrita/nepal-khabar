0c0a0ce2743d68179e721cb3ef3c04ffca179d54
smrita <smriita@gmail.com>
2013-03-29 17:22:49 +0545

    content added to landing pages and others

--- nepal khabar/js/sidertabbedbar.js
+++ nepal khabar/js/sidertabbedbar.js
@@ -12,13 +12,27 @@
                                 if ($(this).html() === "Politics")
                                 {
 
-                                    $('#content_changer').html("<p><a href='#'>Obama reelected</a></p><p><a href='#'>A cat died.</a></p><p><a href='#'> xyz is arrested</a></p><p><a href='#'> will constitution ever be written? </a></p>");
+                                    $('#content_changer').html("<p><a href='NelsonMandelaHealth.html'>Nelson Mandela health: Zuma reassures South Africa</a></p>\
+                                        <p><a href='politicsKimJon>What's Kim Jong Un up to?</a></p><p><a href='opinionBarakObma.html'> Obama signs executive order \n\
+                                            to create commission</a></p><p><a href='politicsKimjon.html'> What's Kim Jong Un up to? </a></p>");
 
                                 }
                                 if ($(this).html() === "Economy")
                                 {
 
-                                    $('#content_changer').html("<p><a href='#'>O </a></p><p><a href='#'>A cat died.</a></p><p><a href='#'> xyz is arrested</a></p><p><a href='#'> will constitution ever be written? </a></p>");
+                                    $('#content_changer').html("<p><a href='#'> Finnish Prime Minister Jyrki Katainen about European economy </a></p>");
+
+                                }
+                                if ($(this).html() === "Sports")
+                                {
+
+                                    $('#content_changer').html("<p><a href='SportNepaliCricket.html'>Hong Kong expose Nepal’s poor batting </a></p><p><a href='#'>India won match </a></p>");
+
+                                }
+                                if ($(this).html() === "Tech")
+                                {
+
+                                    $('#content_changer').html("<p><a href='techGoogle.html'>Google and Ogooglebar: Who owns the meaning of words?</a></p>");
 
                                 }
                             });
